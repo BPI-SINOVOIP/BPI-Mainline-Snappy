5bb57a805263b553c5ece4675b05ee1021d5cac8
August <mingxin.android@gmail.com>
2015-09-01 12:38:03 +0800

    add extra kernel config for snappy required to fix the seccomp_load failed with -22 issue

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ cd extracted && snappy build -o ../
 cd -
 
 sudo ubuntu-device-flash core 15.04 \
+    --size 4 \
+    --channel edge \
+    --enable-ssh \
     --oem bpi-m2_0.01_all.snap \
     --developer-mode \
     --device-part=device.tar.xz \
