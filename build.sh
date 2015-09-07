b1337d42a407df50cfa219954fe799e36c78ef16
August <mingxin.android@gmail.com>
2015-09-07 18:21:13 +0800

    update m2 snappy to v0.2

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ sudo ubuntu-device-flash core 15.04 \
     --size 4 \
     --channel edge \
     --enable-ssh \
-    --oem bpi-m2_0.01_all.snap \
+    --oem bananapi_0.2_all.snap \
     --developer-mode \
     --device-part=device.tar.xz \
+    --install=webdm \
+    --install=docker \
     -o my_bananpi_m2.img
